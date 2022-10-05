f870e8c60a4b90382ba2ed0c40eaf7f2e18629a4
Bobbyshu <shuyunmu@163.com>
2022-10-04 23:48:36 -0400

    update

--- src/A_OUEI/douyin/ChongPaiArr.java
+++ src/A_OUEI/douyin/ChongPaiArr.java
@@ -2,10 +2,10 @@ package A_OUEI.douyin;
 
 // coding1: 一个数组a，可以给其中任意个数加上1然后对a重排列，问能否等于另一个数组b
 public class ChongPaiArr {
-    boolean rearrange(int[] a, int[] b) {
-        if (a.length != b.length)
-            return false;
-
+    boolean rearrange(String a, String b) {
+//        if (a.length != b.length)
+//            return false;
+//
         return true;
     }
 }
