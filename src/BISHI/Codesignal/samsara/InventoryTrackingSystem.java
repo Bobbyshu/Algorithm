75fb3e4379f2dee91f93fc34736bd96ae4511679
Bobbyshu <shuyunmu@163.com>
2023-10-22 17:56:18 -0400

    Inventory

--- src/BISHI/Codesignal/samsara/InventoryTrackingSystem.java
+++ src/BISHI/Codesignal/samsara/InventoryTrackingSystem.java
@@ -1,41 +1,37 @@
 package BISHI.Codesignal.samsara;
 
+import javafx.scene.layout.Priority;
+
 import java.util.*;
 
 public class InventoryTrackingSystem {
-    public static void main(String[] args) {
-        String[][] logs = {{"supply", "item1", "2", "100"},
-                            {"supply", "item2", "3", "60"},
-                            {"sell", "item1", "1"},
-                            {"sell", "item1", "1"},
-                {"sell", "item2", "2"},
-                {"return", "item2", "1", "60", "40"},
-                {"sell", "item2", "1"},
-                {"sell", "item2", "1"}};
-        System.out.println(Arrays.toString(solution(logs)));
-    }
-
     static int[] solution(String[][] logs) {
-        Map<String, Integer> map = new HashMap<>();
-        Map<String, Integer> discountMap = new HashMap<>();
+        Map<String, PriorityQueue<Integer>> map = new HashMap<>();
         List<Integer> list = new ArrayList<>();
-        int discount = 0;
 
         for (String[] s : logs) {
             if (s[0].equals("supply")) {
-                map.put(s[1], Integer.valueOf(s[3]));
-            } else if (s[0].equals("return")) {
-                discountMap.put(s[1], Integer.valueOf(s[4]));
-                discount = Integer.parseInt(s[2]);
-            } else {
-                if (discount > 0) {
-                    list.add(discountMap.get(s[1]));
-                    discount--;
-                } else {
-                    list.add(Integer.parseInt(s[2]) * map.get(s[1]));
+                map.putIfAbsent(s[1], new PriorityQueue<>());
+                for (int i = 0; i < Integer.parseInt(s[2]); ++i) {
+                    map.get(s[1]).offer(Integer.valueOf(s[3]));
                 }
+            } else if (s[0].equals("return")) {
+                map.putIfAbsent(s[1], new PriorityQueue<>());
+                for (int i = 0; i < Integer.parseInt(s[2]); ++i) {
+                    map.get(s[1]).offer(Integer.valueOf(s[4]));
+                }
+            } else {
+                int require = Integer.parseInt(s[2]);
+                int cnt = 0;
+                while (require > 0) {
+                    int cur = map.get(s[1]).poll();
+                    cnt += cur;
+                    --require;
+                }
+                list.add(cnt);
             }
         }
+
         int[] res = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
             res[i] = list.get(i);
