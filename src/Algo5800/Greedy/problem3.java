38e9357c8069336ea431af1f4a837b2d050c9487
Bobbyshu <shu.yun@northeastern.edu>
2023-11-09 00:57:03 -0500

    list

--- src/Algo5800/Greedy/problem3.java
+++ src/Algo5800/Greedy/problem3.java
@@ -1,6 +1,8 @@
 package Algo5800.Greedy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class problem3 {
   public static void main(String[] args) {
@@ -8,34 +10,34 @@ public class problem3 {
     System.out.println(coloringIntervals(arr2));
   }
   public static int coloringIntervals(int[][] x) {
-    Arrays.sort(x, (a, b) -> {
-      if (a[0] != b[0]) return a[0] - b[0];
-      return b[1] - a[1];
-    });
+    Arrays.sort(x, (o1, o2) -> o1[0] == o2[0] ? o2[1] - o1[1] : o1[0] - o2[0]);
 
-    int[] lastEndOfColor = new int[x.length];
-    lastEndOfColor[0] = x[0][1];
-
-    int numColor = 1;
+//    int[] lastEndOfColor = new int[x.length];
+    List<Integer> lastEndOfColor = new ArrayList<>();
+//    lastEndOfColor[0] = x[0][1];
+    lastEndOfColor.add(x[0][1]);
+//    int numColor = 1;
 
     for (int i = 1; i < x.length; i++) {
       int[] currInterval = x[i];
       boolean foundPrevColor = false;
-      for (int j = 0; j < numColor; j++) {
+      for (int j = 0; j < lastEndOfColor.size(); j++) {
         // no overlap. reuse a previous color
-        if (currInterval[0] >= lastEndOfColor[j]) {
-          lastEndOfColor[j] = currInterval[1];
+        if (currInterval[0] >= lastEndOfColor.get(j)) {
+//          lastEndOfColor[j] = currInterval[1];
+          lastEndOfColor.set(j, currInterval[1]);
           foundPrevColor = true;
           break;
         }
       }
 
       if (!foundPrevColor) {
-        lastEndOfColor[numColor] = currInterval[1];
-        numColor++;
+//        lastEndOfColor[numColor] = currInterval[1];
+//        numColor++;
+        lastEndOfColor.add(currInterval[1]);
       }
     }
 
-    return numColor;
+    return lastEndOfColor.size();
   }
 }
