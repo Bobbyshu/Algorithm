5866ab206451f88965da042c4a52ccc370eedf9d
Bobbyshu <shu.yun@northeastern.edu>
2023-12-15 21:47:38 -0500

    modify

--- src/Algo5800/Graph_Advanced/bellman.java
+++ src/Algo5800/Graph_Advanced/bellman.java
@@ -1,13 +1,11 @@
 package Algo5800.Graph_Advanced;// A Java program for Bellman-Ford's single source shortest
 // path algorithm.
 
-import java.io.*;
 import java.lang.*;
-import java.util.*;
 
 // A class to represent a connected, directed and weighted
 // graph
-class Graph {
+class bellman {
 
   // A class to represent a weighted edge in graph
   class Edge {
@@ -19,7 +17,7 @@ class Graph {
   Edge edge[];
 
   // Creates a graph with V vertices and E edges
-  Graph(int v, int e)
+  bellman(int v, int e)
   {
     V = v;
     E = e;
@@ -32,9 +30,9 @@ class Graph {
   // src to all other vertices using Bellman-Ford
   // algorithm. The function also detects negative weight
   // cycle
-  void BellmanFord(Graph graph, int src)
+  void BellmanFord(bellman bellman, int src)
   {
-    int V = graph.V, E = graph.E;
+    int V = bellman.V, E = bellman.E;
     int dist[] = new int[V];
 
     // Step 1: Initialize distances from src to all
@@ -48,9 +46,9 @@ class Graph {
     // have at-most |V| - 1 edges
     for (int i = 1; i < V; ++i) {
       for (int j = 0; j < E; ++j) {
-        int u = graph.edge[j].src;
-        int v = graph.edge[j].dest;
-        int weight = graph.edge[j].weight;
+        int u = bellman.edge[j].src;
+        int v = bellman.edge[j].dest;
+        int weight = bellman.edge[j].weight;
         if (dist[u] != Integer.MAX_VALUE
             && dist[u] + weight < dist[v])
           dist[v] = dist[u] + weight;
@@ -62,9 +60,9 @@ class Graph {
     // doesn't contain negative weight cycle. If we get
     // a shorter path, then there is a cycle.
     for (int j = 0; j < E; ++j) {
-      int u = graph.edge[j].src;
-      int v = graph.edge[j].dest;
-      int weight = graph.edge[j].weight;
+      int u = bellman.edge[j].src;
+      int v = bellman.edge[j].dest;
+      int weight = bellman.edge[j].weight;
       if (dist[u] != Integer.MAX_VALUE
           && dist[u] + weight < dist[v]) {
         System.out.println(
@@ -89,50 +87,50 @@ class Graph {
     int V = 5; // Number of vertices in graph
     int E = 8; // Number of edges in graph
 
-    Graph graph = new Graph(V, E);
+    bellman bellman = new bellman(V, E);
 
     // add edge 0-1 (or A-B in above figure)
-    graph.edge[0].src = 0;
-    graph.edge[0].dest = 1;
-    graph.edge[0].weight = -1;
+    bellman.edge[0].src = 0;
+    bellman.edge[0].dest = 1;
+    bellman.edge[0].weight = -1;
 
     // add edge 0-2 (or A-C in above figure)
-    graph.edge[1].src = 0;
-    graph.edge[1].dest = 2;
-    graph.edge[1].weight = 4;
+    bellman.edge[1].src = 0;
+    bellman.edge[1].dest = 2;
+    bellman.edge[1].weight = 4;
 
     // add edge 1-2 (or B-C in above figure)
-    graph.edge[2].src = 1;
-    graph.edge[2].dest = 2;
-    graph.edge[2].weight = 3;
+    bellman.edge[2].src = 1;
+    bellman.edge[2].dest = 2;
+    bellman.edge[2].weight = 3;
 
     // add edge 1-3 (or B-D in above figure)
-    graph.edge[3].src = 1;
-    graph.edge[3].dest = 3;
-    graph.edge[3].weight = 2;
+    bellman.edge[3].src = 1;
+    bellman.edge[3].dest = 3;
+    bellman.edge[3].weight = 2;
 
     // add edge 1-4 (or B-E in above figure)
-    graph.edge[4].src = 1;
-    graph.edge[4].dest = 4;
-    graph.edge[4].weight = 2;
+    bellman.edge[4].src = 1;
+    bellman.edge[4].dest = 4;
+    bellman.edge[4].weight = 2;
 
     // add edge 3-2 (or D-C in above figure)
-    graph.edge[5].src = 3;
-    graph.edge[5].dest = 2;
-    graph.edge[5].weight = 5;
+    bellman.edge[5].src = 3;
+    bellman.edge[5].dest = 2;
+    bellman.edge[5].weight = 5;
 
     // add edge 3-1 (or D-B in above figure)
-    graph.edge[6].src = 3;
-    graph.edge[6].dest = 1;
-    graph.edge[6].weight = 1;
+    bellman.edge[6].src = 3;
+    bellman.edge[6].dest = 1;
+    bellman.edge[6].weight = 1;
 
     // add edge 4-3 (or E-D in above figure)
-    graph.edge[7].src = 4;
-    graph.edge[7].dest = 3;
-    graph.edge[7].weight = -3;
+    bellman.edge[7].src = 4;
+    bellman.edge[7].dest = 3;
+    bellman.edge[7].weight = -3;
 
     // Function call
-    graph.BellmanFord(graph, 0);
+    bellman.BellmanFord(bellman, 0);
   }
 }
 // Contributed by Aakash Hasija
