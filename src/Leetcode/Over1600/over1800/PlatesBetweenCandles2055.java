4174af75f0515363b77f1b51c872618d6b65cdec
Bobbyshu <shu.yun@northeastern.edu>
2024-06-12 15:24:10 -0400

    预处理+前缀和

--- src/Leetcode/Over1600/over1800/PlatesBetweenCandles2055.java
+++ src/Leetcode/Over1600/over1800/PlatesBetweenCandles2055.java
@@ -1,9 +1,49 @@
 package Leetcode.Over1600.over1800;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PlatesBetweenCandles2055 {
+    public int[] platesBetweenCandlesPrefix(String s, int[][] queries) {
+        int n = s.length(), m = queries.length;
+        int[] left = new int[n], right = new int[n];
+
+        Arrays.fill(left, -1);
+        Arrays.fill(right, -1);
+
+        left[0] = s.charAt(0) == '|' ? 0 : -1;
+        for (int i = 1; i < n; ++i) {
+            left[i] = s.charAt(i) == '|' ? i : left[i - 1];
+        }
+
+        right[n - 1] = s.charAt(n - 1) == '|' ? n - 1 : -1;
+        for (int i = n - 2; i >= 0; --i) {
+            right[i] = s.charAt(i) == '|' ? i : right[i + 1];
+        }
+
+        // System.out.println(Arrays.toString(left));
+        // System.out.println(Arrays.toString(right));
+
+        int[] prefix = new int[n];
+        prefix[0] = s.charAt(0) == '*' ? 1 : 0;
+        for (int i = 1; i < n; ++i) {
+            prefix[i] = s.charAt(i) == '*' ? prefix[i - 1] + 1 : prefix[i - 1];
+        }
+
+        int[] res = new int[m];
+        for (int i = 0; i < m; ++i) {
+            int[] q = queries[i];
+            int leftCandle = right[q[0]], rightCandle = left[q[1]];
+            if (leftCandle == -1 || rightCandle == -1 || leftCandle >= rightCandle) {
+                continue;
+            }
+
+            res[i] = prefix[rightCandle] - prefix[leftCandle];
+        }
+
+        return res;
+    }
     public int[] platesBetweenCandles(String s, int[][] queries) {
         List<Integer> lst = new ArrayList<>();
         int n = s.length(), m = queries.length;
