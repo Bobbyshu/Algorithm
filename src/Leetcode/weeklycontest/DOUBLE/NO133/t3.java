6e3653fa82c8afb84fba2bf6aa998a855318bae5
Bobbyshu <shu.yun@northeastern.edu>
2024-06-23 12:22:58 -0400

    反转奇偶性

--- src/Leetcode/weeklycontest/DOUBLE/NO133/t3.java
+++ src/Leetcode/weeklycontest/DOUBLE/NO133/t3.java
@@ -1,4 +1,14 @@
 package Leetcode.weeklycontest.DOUBLE.NO133;
 
 public class t3 {
+  // 是否需要反转和翻转次数奇偶性有关
+  public int minOperations(int[] nums) {
+    int res = 0;
+    for (int n : nums) {
+      if (n == res % 2) {
+        ++res;
+      }
+    }
+    return res;
+  }
 }
