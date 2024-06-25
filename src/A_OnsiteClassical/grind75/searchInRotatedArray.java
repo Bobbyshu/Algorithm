785e5238dadd165ed60db429964f1bfbd1bdf27a
Bobbyshu <shu.yun@northeastern.edu>
2024-06-25 14:33:44 -0400

    二分最基础版

--- src/A_OnsiteClassical/grind75/searchInRotatedArray.java
+++ src/A_OnsiteClassical/grind75/searchInRotatedArray.java
@@ -1,6 +1,22 @@
 package A_OnsiteClassical.grind75;
 
 public class searchInRotatedArray {
+  public int search(int[] nums, int target) {
+    int l = 0, r = nums.length - 1;
+    // 基础版二分查找，要注意这里是 <= ，计算入l == r的情况
+    while (l <= r) {
+      int mid = l + (r - l) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] < target) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    }
+
+    return -1;
+  }
   // 4 5 6 7 8 9 10 11 12 13 14 0 1 2
   //
   public int compareFirstElement(int[] nums, int target) {
@@ -36,4 +52,6 @@ public class searchInRotatedArray {
 
     return nums[l] == target ? l : -1;
   }
+
+
 }
