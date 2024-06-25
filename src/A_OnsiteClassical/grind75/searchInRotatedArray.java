ec56d2b56e110522957326dd13d529e60f0fdc25
Bobbyshu <shu.yun@northeastern.edu>
2024-06-25 14:51:18 -0400

    和二分的左边界比较

--- src/A_OnsiteClassical/grind75/searchInRotatedArray.java
+++ src/A_OnsiteClassical/grind75/searchInRotatedArray.java
@@ -17,6 +17,7 @@ public class searchInRotatedArray {
 
     return -1;
   }
+
   // 4 5 6 7 8 9 10 11 12 13 14 0 1 2
   //
   public int compareFirstElement(int[] nums, int target) {
@@ -53,5 +54,36 @@ public class searchInRotatedArray {
     return nums[l] == target ? l : -1;
   }
 
+  public int compareLeftBoundary(int[] nums, int target) {
+    int l = 0, r = nums.length - 1;
+    while (l <= r) {
+      int mid = l + (r - l) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      }
 
+      // because it will always has one side within mid in order
+      // when we find that side
+      // we can check whether target located in that side or not
+      // and change the left/right boundary based on location
+      // in left side
+      if (nums[mid] >= nums[l]) {
+        // target located between nums[l] and nums[mid]
+        if (target >= nums[l] && target < nums[mid]) {
+          r = mid - 1;
+        } else { // otherwise it should located at rightside of nums[mid]
+          l = mid + 1;
+        }
+      } else { // nums[mid] < nums[l]
+        // located at right side
+        if (target > nums[mid] && target < nums[l]) {
+          l = mid + 1;
+        } else {
+          r = mid - 1;
+        }
+      }
+    }
+
+    return -1;
+  }
 }
