c280e7a3c1e39d3c7f4df646e7748367b2cd28ef
Bobbyshu <shu.yun@northeastern.edu>
2024-07-14 19:27:03 -0400

    复习链表删除操作

---
+++ src/A_OnsiteClassical/grind75/wordBreak.java
@@ -0,0 +1,4 @@
+package A_OnsiteClassical.grind75;
+
+public class wordBreak {
+}
---
+++ src/Leetcode/weeklycontest/Normal/NO406/t2.java
@@ -0,0 +1,41 @@
+package Leetcode.weeklycontest.Normal.NO406;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class t2 {
+  public ListNode modifiedList(int[] nums, ListNode head) {
+    Set<Integer> set = new HashSet<>();
+    for (int n : nums) {
+      set.add(n);
+    }
+
+    ListNode dummy = new ListNode(0, head);
+    ListNode cur = dummy;
+    while (cur.next != null) {
+      if (set.contains(cur.next.val)) {
+        cur.next = cur.next.next;
+      } else {
+        cur = cur.next;
+      }
+    }
+    return dummy.next;
+  }
+
+  public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+}
