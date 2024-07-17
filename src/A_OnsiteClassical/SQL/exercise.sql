64eb4d3abdd8abc3e58fd8ef59b97d35dc97af7f
Bobby Shu <101524606+Bobbyshu@users.noreply.github.com>
2024-07-17 14:57:42 -0400

    Update exercise.sql

--- src/A_OnsiteClassical/SQL/exercise.sql
+++ src/A_OnsiteClassical/SQL/exercise.sql
@@ -4,3 +4,13 @@
 SELECT DISTINCT CITY
 FROM STATION
 WHERE CITY RLIKE '^[AEIOU]' AND CITY RLIKE '[aeiou]$';
+
+-- Query the Name of any student in STUDENTS who scored higher than  Marks. 
+-- Order your output by the last three characters of each name. 
+-- If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), 
+-- secondary sort them by ascending ID.
+-- sort use comma separate and Right function to get character from string
+select name
+from students
+where marks > 75
+order by RIGHT(name, 3), ID asc
