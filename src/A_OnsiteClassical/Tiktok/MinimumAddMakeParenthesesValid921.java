26aafc7d8df31ef9edc6e184674ba6123a97e061
Bobbyshu <shu.yun@northeastern.edu>
2024-08-10 01:22:42 -0400

    不能粗心

---
+++ src/A_OnsiteClassical/Tiktok/MinimumAddMakeParenthesesValid921.java
@@ -0,0 +1,22 @@
+package A_OnsiteClassical.Tiktok;
+
+public class MinimumAddMakeParenthesesValid921 {
+  // 要考虑)在(前面的情况
+  public int minAddToMakeValid(String s) {
+    int left = 0, right = 0;
+    for (char c : s.toCharArray()) {
+      if (c == '(') {
+        ++left;
+      } else {
+        if (left > 0) {
+          --left;
+        } else {
+          // can't be offset
+          ++right;
+        }
+      }
+      // System.out.println(cnt);
+    }
+    return left + right;
+  }
+}
