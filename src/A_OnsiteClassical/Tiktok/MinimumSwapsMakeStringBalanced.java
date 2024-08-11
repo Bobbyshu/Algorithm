32afc0c3bf4d2ebaa250db70b747d90cc045b9fd
Bobbyshu <shu.yun@northeastern.edu>
2024-08-11 00:41:07 -0400

    greedy

---
+++ src/A_OnsiteClassical/Tiktok/MinimumSwapsMakeStringBalanced.java
@@ -0,0 +1,28 @@
+package A_OnsiteClassical.Tiktok;
+
+public class MinimumSwapsMakeStringBalanced {
+  // to make min swap
+  // we need to always swap the ']' to the rightMost index
+  // cuz rightMost can make more legal []
+  public int minSwaps(String s) {
+    int left = 0, res = 0;
+    for (char c : s.toCharArray()) {
+      if (c == '[') {
+        ++left;
+      } else {
+        if (left > 0) {
+          --left;
+        } else {
+          // equals to we swap the rightMost [
+          // to current illegal ]
+          // so the left [ increase
+          // swap times ++
+          ++left;
+          ++res;
+        }
+      }
+    }
+
+    return res;
+  }
+}
