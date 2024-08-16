7aea19a062725dd381ef14dae990f70573a0faf8
Bobbyshu <shu.yun@northeastern.edu>
2024-08-15 23:04:20 -0400

    0-1背包恰好装满

--- src/A_OnsiteClassical/Tiktok/LongestSubsequenceSumTarget.java
+++ src/A_OnsiteClassical/Tiktok/LongestSubsequenceSumTarget.java
@@ -11,13 +11,14 @@ public class LongestSubsequenceSumTarget {
     dp[0] = 0;
 
     for (int i = 0; i < nums.size(); ++i) {
+      // 这里一定要倒序遍历这样才可以得出合理的结果
       for (int j = target; j >= nums.get(i); --j) {
-        if (dp[j - nums.get(i)] != Integer.MIN_VALUE) {
-          dp[j] = Math.max(dp[j], dp[j - nums.get(i)] + 1);
-        }
+//        if (dp[j - nums.get(i)] != Integer.MIN_VALUE) {
+        dp[j] = Math.max(dp[j], dp[j - nums.get(i)] + 1);
+//        }
       }
     }
 
-    return dp[target] == Integer.MIN_VALUE ? -1 : dp[target];
+    return Math.max(-1, dp[target]);
   }
 }
