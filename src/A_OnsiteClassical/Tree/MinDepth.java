8b2872e77ff745e21311e910e86f6e602b36a2b5
Bobbyshu <shu.yun@northeastern.edu>
2024-10-12 20:01:45 -0400

    最小深度二叉树

--- src/A_OnsiteClassical/Tree/MinDepth.java
+++ src/A_OnsiteClassical/Tree/MinDepth.java
@@ -2,23 +2,23 @@ package A_OnsiteClassical.Tree;
 
 public class MinDepth {
     // 强调是到叶子节点，所以如果一边为空则不能算到了
-//    public int minDepth(TreeNode root) {
-//        if (root == null) {
-//            return 0;
-//        }
-//
-//        if (root.left == null && root.right == null) {
-//            return 1;
-//        }
-//
-//        if (root.right == null) {
-//            return minDepth(root.left) + 1;
-//        }
-//
-//        if (root.left == null) {
-//            return minDepth(root.right) + 1;
-//        }
-//
-//        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
-//    }
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
 }
