4124da874aac40ddafc7d8bb45c26dabd2e934bd
Bobbyshu <shu.yun@northeastern.edu>
2024-10-27 19:48:38 -0400

    回溯分割型子集

---
+++ src/A_OnsiteClassical/Tiktok/SplitMaxNumberUniqueSubstring1593.java
@@ -0,0 +1,36 @@
+package A_OnsiteClassical.Tiktok;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SplitMaxNumberUniqueSubstring1593 {
+  int res = 0;
+
+  public int maxUniqueSplit(String s) {
+    Set<String> set = new HashSet<>();
+    dfs(s, 0, set);
+    return res;
+  }
+
+  private void dfs(String s, int pos, Set<String> set) {
+    if (s.length() - pos + set.size() <= res) {
+      return;
+    }
+
+    if (pos == s.length()) {
+      res = Math.max(res, set.size());
+      return;
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = pos; i < s.length(); ++i) {
+      sb.append(s.charAt(i));
+      String cur = sb.toString();
+      if (!set.contains(cur)) {
+        set.add(cur);
+        dfs(s, i + 1, set);
+        set.remove(cur);
+      }
+    }
+  }
+}
