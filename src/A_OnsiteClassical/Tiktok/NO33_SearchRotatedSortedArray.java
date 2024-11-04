0327231479476e6491d2f0d18993646d29d33a98
Bobbyshu <shu.yun@northeastern.edu>
2024-11-03 20:14:06 -0500

    二分搜索

--- src/A_OnsiteClassical/Tiktok/NO33_SearchRotatedSortedArray.java
+++ src/A_OnsiteClassical/Tiktok/NO33_SearchRotatedSortedArray.java
@@ -1,6 +1,34 @@
 package A_OnsiteClassical.Tiktok;
 
 public class NO33_SearchRotatedSortedArray {
+  public int search2(int[] nums, int target) {
+    int l = 0, r = nums.length - 1;
+    while (l < r) {
+      int mid = l + (r - l) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      }
+
+      // in left side
+      if (nums[mid] >= nums[l]) {
+        // target located between nums[l] and nums[mid]
+        if (target >= nums[l] && target < nums[mid]) {
+          r = mid;
+        } else { // otherwise it should located at rightside of nums[mid]
+          l = mid + 1;
+        }
+      } else { // nums[mid] < nums[l]
+        // located at right side
+        if (target > nums[mid] && target < nums[l]) {
+          l = mid + 1;
+        } else {
+          r = mid;
+        }
+      }
+    }
+
+    return nums[r] == target ? r : -1;
+  }
 
   public int search(int[] nums, int target) {
     int l = 0, r = nums.length - 1;
